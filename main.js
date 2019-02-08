c8badc467696c5f5f7e803e6ec63d4008b8b883c
Lisa Lemons - Grand Circus <LisaLemonsGrandCircus@Katies-MacBook-Air.local>
2019-02-07 22:08:00 -0500

    Added a car to the arrayOfCars

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const schmittysPad = {
+const SCHMITTYS_PAD = {
     name: 'apartment',
     floors: 1,
     sqfeet: 1200,
@@ -8,24 +8,41 @@ const schmittysPad = {
     baths: 1,
     yearBuilt: 1989
 };
-const allistair = {
+const ALLISTAIR = {
     make: 'Infiniti',
     model: 'Q50',
     year: '2018'
 };
+const CHERRY_BOMB = {
+    make: 'Toyota',
+    model: 'Rav4',
+    year: '2008'
+};
 
-for (let property of Object.keys(allistair)) {
+let arrayOfCars = [ALLISTAIR, CHERRY_BOMB];
+
+for (let i = 0; i < arrayOfCars.length; i++) {
     let carContainer = document.getElementById('car'),
         carPropertyListItem = document.createElement('li');
 
-    carPropertyListItem.appendChild(document.createTextNode(`${property}: ${allistair[property]}`));
+    carPropertyListItem.appendChild(document.createTextNode(`Make: ${arrayOfCars[i].make}`));
+    carPropertyListItem.appendChild(document.createTextNode(`Model: ${arrayOfCars[i].model}`));
+    carPropertyListItem.appendChild(document.createTextNode(`Year: ${arrayOfCars[i].year}`));
     carContainer.appendChild(carPropertyListItem);
 }
 
-for (let property of Object.keys(schmittysPad)) {
+for (let property of Object.keys(ALLISTAIR)) {
+    let carContainer = document.getElementById('car'),
+        carPropertyListItem = document.createElement('li');
+
+    carPropertyListItem.appendChild(document.createTextNode(`${property}: ${ALLISTAIR[property]}`));
+    carContainer.appendChild(carPropertyListItem);
+}
+
+for (let property of Object.keys(SCHMITTYS_PAD)) {
     let houseContainer = document.getElementById('house'),
         housePropertyListItem = document.createElement('li');
 
-    housePropertyListItem.appendChild(document.createTextNode(`${property}: ${schmittysPad[property]}`));
+    housePropertyListItem.appendChild(document.createTextNode(`${property}: ${SCHMITTYS_PAD[property]}`));
     houseContainer.appendChild(housePropertyListItem);
 }
